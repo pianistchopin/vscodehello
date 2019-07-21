a416c6037596cf8369e68e22142789421d77ae91
jinzhuge <52116801+jinzhuge@users.noreply.github.com>
2019-07-21 20:39:09 +0800

    gps_hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,8 +11,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      // please get your own API key here:
+      // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
+      apiKey: 'AIzaSyCQ27LsC-CaWjVXPQZOgYffjKMsk-Zmz1o'
+    })
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
